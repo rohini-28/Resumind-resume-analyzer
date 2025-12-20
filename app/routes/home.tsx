1621ad52ed8ff4c5931117952aece32ed513e1e7
rohini-28 <rohinikamati87@gmail.com>
2025-12-20 15:30:25 +0530

    Authentication

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,9 @@ import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
 import { resumes } from "../../constants";
 import ResumeCard from "~/components/ResumeCard";
+import React, { useEffect } from 'react'
+import { useLocation, useNavigate } from 'react-router';
+import { usePuterStore } from '~/lib/puter';
 
 
 export function meta({}: Route.MetaArgs) {
@@ -12,7 +15,15 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const {isLoading,auth} =usePuterStore();
+  const navigate=useNavigate();
+
+  useEffect(()=>{
+    if(!auth.isAuthenticated) navigate('/auth?next=/');
+  },[auth.isAuthenticated])
+
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    
     <Navbar />
     <section className="main-section">
       <div className="page-heading py-16">
